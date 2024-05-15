56ea9fcdb645b737546d08481445980dad15d564
Prajwal Ghimire <58899020+prajwal60@users.noreply.github.com>
2024-05-15 09:00:10 +0545

    upload

--- Tasmanian/Tasmanian/settings.py
+++ Tasmanian/Tasmanian/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +35,7 @@ EMAIL_pORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = ['.vercel.app','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
